1951a0dc851350352f19f9b8fdbbe882e7979410
Abhik Das <abhikdas98765@gmail.com>
2022-04-13 22:41:02 +0530

    added code for image carousel

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const btn = document.getElementById("btn");
-const nav = document.getElementById("nav");
+const images = document.getElementById("images");
 
-btn.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  btn.classList.toggle("active");
-});
+const allImgs = images.querySelectorAll("img");
+let idx = 0;
+
+function slideshow() {
+  idx++;
+  if (idx > allImgs.length - 1) idx = 0;
+  images.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+setInterval(slideshow, 2000);
